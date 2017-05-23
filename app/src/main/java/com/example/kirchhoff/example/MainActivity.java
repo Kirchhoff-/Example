2220917983c11ec28a4083515f3a96dde9ef42ae
Kirchhoff- <kirchhoff.19.92@gmail.com>
2017-05-23 22:23:35 +0300

    Add example using RecyclerView with SearchView.

--- app/src/main/java/com/example/kirchhoff/example/MainActivity.java
+++ app/src/main/java/com/example/kirchhoff/example/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.kirchhoff.example;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.kirchhoff.example.ui.recyclersearch.RecyclerSearchActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.a_main);
+
+        findViewById(R.id.recyclerViewWithSearch).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.recyclerViewWithSearch)
+            RecyclerSearchActivity.startMe(this);
     }
 }
---
+++ app/src/main/java/com/example/kirchhoff/example/ui/recyclersearch/data/JSONResponse.java
@@ -0,0 +1,13 @@
+package com.example.kirchhoff.example.ui.recyclersearch.data;
+
+/**
+ * @author Kirchhoff-
+ */
+
+public class JSONResponse {
+    private AndroidVersion[] android;
+
+    public AndroidVersion[] getAndroid() {
+        return android;
+    }
+}
