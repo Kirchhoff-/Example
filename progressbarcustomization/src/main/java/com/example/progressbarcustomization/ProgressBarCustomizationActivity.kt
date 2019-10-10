53ee93b0e7969f1a0634b0e278b3d40546126e72
Kirchhoff- <kirchhoff.19.92@gmail.com>
2019-10-10 20:52:34 +0300

    Issue-207: Added example of vertical progressBar.

--- progressbarcustomization/src/main/java/com/example/progressbarcustomization/ProgressBarCustomizationActivity.kt
+++ progressbarcustomization/src/main/java/com/example/progressbarcustomization/ProgressBarCustomizationActivity.kt
@@ -10,6 +10,7 @@ class ProgressBarCustomizationActivity : BaseActivity() {
 
     private lateinit var pbFirst: ProgressBar
     private lateinit var pbSecond: ProgressBar
+    private lateinit var pbThird: ProgressBar
 
     private val handler = Handler()
     private val progressRunnable = ProgressRunnable()
@@ -19,6 +20,7 @@ class ProgressBarCustomizationActivity : BaseActivity() {
 
         pbFirst = findViewById(R.id.pbFirst)
         pbSecond = findViewById(R.id.pbSecond)
+        pbThird = findViewById(R.id.pbThird)
         findViewById<Button>(R.id.bStartProgress).setOnClickListener { startProgress() }
     }
 
@@ -41,8 +43,10 @@ class ProgressBarCustomizationActivity : BaseActivity() {
 
             pbFirst.progress = progress
             pbSecond.progress = progress
+            pbThird.progress = progress
             progress += 10
             pbFirst.secondaryProgress = progress
+            pbThird.secondaryProgress = progress
 
             if (progress == 110) {
                 handler.removeCallbacks(this)
