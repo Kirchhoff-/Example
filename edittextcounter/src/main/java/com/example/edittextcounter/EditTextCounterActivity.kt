66b229fc65804833ae0b237603551d404c02c0e7
Kirchhoff- <kirchhoff.19.92@gmail.com>
2019-12-03 09:54:59 +0200

    Issue-241: Added EditText with symbol counter example.

---
+++ edittextcounter/src/main/java/com/example/edittextcounter/EditTextCounterActivity.kt
@@ -0,0 +1,8 @@
+package com.example.edittextcounter
+
+import com.example.core.ui.BaseActivity
+import com.example.custommenuitem.R
+
+class EditTextCounterActivity : BaseActivity() {
+    override fun getLayoutId() = R.layout.a_edit_text_counter
+}
