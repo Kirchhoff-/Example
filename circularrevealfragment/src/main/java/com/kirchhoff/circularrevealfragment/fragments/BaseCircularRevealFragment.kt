f3dd4362cb4c1b9566311e7388d54f5e6297370d
Kirchhoff- <kirchhoff.19.92@gmail.com>
2019-12-05 09:53:12 +0200

    Issue-269: Make BaseCircularRevealFragment inherit from BaseFragment.

--- circularrevealfragment/src/main/java/com/kirchhoff/circularrevealfragment/fragments/BaseCircularRevealFragment.kt
+++ circularrevealfragment/src/main/java/com/kirchhoff/circularrevealfragment/fragments/BaseCircularRevealFragment.kt
@@ -1,26 +1,17 @@
 package com.kirchhoff.circularrevealfragment.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.annotation.LayoutRes
-import androidx.fragment.app.Fragment
+import com.example.core.ui.BaseFragment
 import com.kirchhoff.circularrevealfragment.utils.ExitWithAnimation
 import com.kirchhoff.circularrevealfragment.utils.startCircularReveal
 
-internal abstract class BaseCircularRevealFragment : Fragment(), ExitWithAnimation {
+internal abstract class BaseCircularRevealFragment : BaseFragment(), ExitWithAnimation {
 
     override var posX: Int? = null
     override var posY: Int? = null
 
-    @LayoutRes abstract fun layoutId(): Int
-
     abstract fun circularRevealFromLeft(): Boolean
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(layoutId(), container, false)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- circularrevealfragment/src/main/java/com/kirchhoff/circularrevealfragment/fragments/FirstFragment.kt
+++ circularrevealfragment/src/main/java/com/kirchhoff/circularrevealfragment/fragments/FirstFragment.kt
@@ -4,6 +4,6 @@ import com.kirchhoff.circularrevealfragment.R
 
 internal class FirstFragment : BaseCircularRevealFragment() {
     override fun isToBeExitedWithAnimation(): Boolean = true
-    override fun layoutId() = R.layout.f_first
+    override fun getLayoutId() = R.layout.f_first
     override fun circularRevealFromLeft() = false
 }
