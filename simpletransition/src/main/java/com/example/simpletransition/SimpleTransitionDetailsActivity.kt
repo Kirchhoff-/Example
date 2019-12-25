af95af182ad7d0febf4928df1b39d445083cf07d
Kirchhoff- <kirchhoff.19.92@gmail.com>
2019-12-25 12:18:27 +0300

    Issue-325: Added constants for arguments position in SimpleTransitionDetailsActivity.

--- simpletransition/src/main/java/com/example/simpletransition/SimpleTransitionDetailsActivity.kt
+++ simpletransition/src/main/java/com/example/simpletransition/SimpleTransitionDetailsActivity.kt
@@ -19,20 +19,27 @@ class SimpleTransitionDetailsActivity : AppCompatActivity() {
         val array = intent.getStringArrayExtra("parameter")
 
         val layout = findViewById<RelativeLayout>(R.id.layout)
-        layout.setBackgroundColor(Color.parseColor(array[3]))
+        layout.setBackgroundColor(Color.parseColor(array[COLOR_ARG_POSITION]))
 
         val tvHeading = findViewById<TextView>(R.id.tvHeading)
-        tvHeading.text = array[0]
+        tvHeading.text = array[TITLE_ARG_POSITION]
 
         val tvLanguage = findViewById<TextView>(R.id.tvLanguage)
-        tvLanguage.text = array[1]
+        tvLanguage.text = array[LANGUAGE_ARG_POSITION]
 
         tvDesc = findViewById(R.id.tvDesc)
-        tvDesc.text = array[2]
+        tvDesc.text = array[DESCRIPTION_ARG_POSITION]
     }
 
     override fun onBackPressed() {
         tvDesc.visibility = View.GONE
         ActivityCompat.finishAfterTransition(this)
+    }
+
+    companion object {
+        private const val TITLE_ARG_POSITION = 0
+        private const val LANGUAGE_ARG_POSITION = 1
+        private const val DESCRIPTION_ARG_POSITION = 2
+        private const val COLOR_ARG_POSITION = 3
     }
 }
