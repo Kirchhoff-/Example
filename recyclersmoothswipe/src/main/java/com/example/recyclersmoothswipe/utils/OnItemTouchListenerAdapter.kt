0893f7171f804947e8887b5bf5c60948c7ea9cef
Kirchhoff- <kirchhoff.19.92@gmail.com>
2021-08-12 20:05:24 +0300

    Issue-282: Fixed formatting problem in OnItemTouchListenerAdapter.kt

--- recyclersmoothswipe/src/main/java/com/example/recyclersmoothswipe/utils/OnItemTouchListenerAdapter.kt
+++ recyclersmoothswipe/src/main/java/com/example/recyclersmoothswipe/utils/OnItemTouchListenerAdapter.kt
@@ -3,12 +3,13 @@ package com.example.recyclersmoothswipe.utils
 import android.view.MotionEvent
 import androidx.recyclerview.widget.RecyclerView
 
-abstract class OnItemTouchListenerAdapter: RecyclerView.OnItemTouchListener {
-  override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean = false
-  override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
-    // override if required
-  }
-  override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
-    // override if required
-  }
+abstract class OnItemTouchListenerAdapter : RecyclerView.OnItemTouchListener {
+    override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean = false
+    override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
+        // override if required
+    }
+
+    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+        // override if required
+    }
 }
