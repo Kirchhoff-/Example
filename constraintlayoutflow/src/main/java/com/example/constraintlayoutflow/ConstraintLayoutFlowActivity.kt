f284222e628c88264ec9807b59cb99ca0dad130d
Kirchhoff- <kirchhoff.19.92@gmail.com>
2022-03-29 15:44:42 +0300

    Issue-317: Added ConstraintLayoutFlow example.

---
+++ constraintlayoutflow/src/main/java/com/example/constraintlayoutflow/ConstraintLayoutFlowActivity.kt
@@ -0,0 +1,5 @@
+package com.example.constraintlayoutflow
+
+import androidx.appcompat.app.AppCompatActivity
+
+class ConstraintLayoutFlowActivity : AppCompatActivity(R.layout.a_constraint_layout_flow)
