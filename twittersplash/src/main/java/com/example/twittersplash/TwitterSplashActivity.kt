55cc55dfc28a9e1bd47da5bba5485594be73b0ed
Kirchhoff- <kirchhoff.19.92@gmail.com>
2022-04-08 16:28:28 +0300

    Issue-429: Fixed code issues.

--- twittersplash/src/main/java/com/example/twittersplash/TwitterSplashActivity.kt
+++ twittersplash/src/main/java/com/example/twittersplash/TwitterSplashActivity.kt
@@ -6,7 +6,7 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 @SuppressLint("CustomSplashScreen")
-class TwitterSplashActivity: AppCompatActivity(R.layout.a_twitter_splash) {
+class TwitterSplashActivity : AppCompatActivity(R.layout.a_twitter_splash) {
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
@@ -18,14 +18,11 @@ class TwitterSplashActivity: AppCompatActivity(R.layout.a_twitter_splash) {
     @Suppress("DEPRECATION")
     private fun hideSystemUIAndNavigation(activity: Activity) {
         val decorView: View = activity.window.decorView
-        decorView.systemUiVisibility =
-            (
-                View.SYSTEM_UI_FLAG_IMMERSIVE or
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                View.SYSTEM_UI_FLAG_FULLSCREEN
-            )
+        decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE or
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
